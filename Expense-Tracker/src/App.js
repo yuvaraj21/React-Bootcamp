30dd9fea00cdca4ef6d08801af631778e3558c7a
yuvaraj21 <ksyuvaraj84899@gmail.com>
2022-12-06 11:44:55 +0530

    passing data via props

--- Expense-Tracker/src/App.js
+++ Expense-Tracker/src/App.js
@@ -1,7 +1,52 @@
+import ExpenseItem from './component/ExpenseItem';
+
 function App() {
+  const expenses = [
+    {
+      name: 'Food1',
+      price: 60.0,
+      date: 'jan 24 2021',
+    },
+    {
+      name: 'Food2',
+      price: 60.0,
+      date: 'jan 24 2021',
+    },
+    {
+      name: 'Food3',
+      price: 60.0,
+      date: 'jan 24 2021',
+    },
+    {
+      name: 'Food4',
+      price: 60.0,
+      date: 'jan 24 2021',
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Expense Tracker React App</h2>
+      <ExpenseItem
+        title={expenses[0].name}
+        amount={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].name}
+        amount={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].name}
+        amount={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].name}
+        amount={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- Expense-Tracker/src/component/ExpenseItem.js
+++ Expense-Tracker/src/component/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>Product</h2>
-        <div className="expense-item__price">$ 58.6</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
